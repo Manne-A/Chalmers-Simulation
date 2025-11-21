28526c3d1198b4867697075dba7f421aee0d820e
Manne-A <manne.almborg@gmail.com>
2025-11-21 10:20:24 +0100

    setting up unity

---
+++ ChSi_Unity/Assets/Scripts/Player/Movement_Player.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Movement_Player : MonoBehaviour
+{
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
